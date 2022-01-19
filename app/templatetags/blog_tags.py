369345fe62caec1bd3f98db61e364f869c7c90b4
Rustam Mustafoev <rustam.mustafoev01@gmail.com>
2022-01-19 19:55:31 +0500

    Custom tags are added, sidebar has been enhanced

---
+++ app/templatetags/blog_tags.py
@@ -0,0 +1,28 @@
+"""
+Django allows us two helper functions to create a custom template tags:
+- simple_tag (Processes and returns a string)
+- inclusion_tag (Processes and returns a rendered template)
+"""
+from django import template
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('app/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
